73f954907c7f0c5ec835044553561365744c9efd
Rayan <godwersmingclash@gmail.com>
2024-02-06 10:56:21 +0100

    Ajoutez des fichiers projet.

---
+++ cslMotdepasse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libMotdepasse;
+
+namespace cslMotdepasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, mdp, mdp2;
+
+            Console.WriteLine("Saisissez votre login");
+            login = Console.ReadLine();
+            Console.WriteLine("Saisissez votre mot de passe");
+            mdp = Console.ReadLine();
+            Console.WriteLine("Confirmez votre mot de passe");
+            mdp2 = Console.ReadLine();
+            MotdePasse Mdp = new MotdePasse(login, mdp);
+
+            if (Mdp.Vérifier(mdp2))
+            {
+                if (Mdp.Longueur() && (Mdp.estNumeric()) && (Mdp.estMajuscule()) & (Mdp.estSpecial()))
+                {
+
+                    Console.WriteLine("Mot de passe valide");
+
+                }
+                    
+                 //   Console.WriteLine(Mdp.Longueur());
+
+            }
+            else
+            {
+                Console.WriteLine("Les mots de passe ne sont pas identiques, ressaisiez votre mot de passe");
+            }
+           // Console.WriteLine(Mdp.Longueur());
+        }
+    }
+}
---
+++ libMotdepasse/MotdePasse.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace libMotdepasse
+{
+    public class MotdePasse
+    {
+        private string login;
+        private string mdp;
+
+        public MotdePasse(string login, string mdp)
+        {
+            this.login = login;
+            this.mdp = mdp;
+        }
+
+        public bool Vérifier(string mdp2)
+        {
+            return mdp == mdp2;
+        }
+
+
+        public bool Longueur()
+        {
+            return mdp.Length >= 12; 
+        }
+
+
+        /* estNumeric Utilise la fonction char is number*/
+
+        public bool estNumeric()
+        {
+            foreach (char c in mdp)
+            {
+                if (char.IsNumber(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /* estMajuscule Utilise la fonction char is upper*/
+
+
+        public bool estMajuscule()
+        {
+            foreach (char c in mdp)
+            {
+                if (char.IsUpper(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    
+
+
+    /* EstSpecial Utilise la fonction char is letter or digit*/
+
+    public bool estSpecial()
+        {
+            foreach (char c in mdp)
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
+
